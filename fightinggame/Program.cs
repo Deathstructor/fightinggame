413fe9929755275d2825346b0e7c704d6a70341b
Paul Chastain <paulchast1@gmail.com>
2021-11-04 23:32:19 +0100

    fullt fungerande deserialisering (äntligen)

--- fightinggame/Program.cs
+++ fightinggame/Program.cs
@@ -1,58 +1,51 @@
-﻿using System.Collections.Generic;
-using System;
+﻿using System;
 using System.IO;
+using System.Collections.Generic;
+using System.Threading;
 using System.Text.Json;
 using System.Text.Json.Serialization;
-using System.Threading;
 
 
 namespace fightinggame
 {
-    // public class EnemyCollection
-    // {
-    //     public enemies[] enemies { get; set; }
-    // }
-    public class player
+    public class User
+    {
+        public Player player { get; set; }
+    }
+    public class Player
     {
         public string name { get; set; }
         public int health { get; set; }
-        public int min_damage { get; set; }
         public int max_damage { get; set; }
+        public int min_damage { get; set; }
     }
-    public class enemies
+
+    public class EnemyTypes
+    {
+        [JsonPropertyName("enemies")]
+        public EnemyCollection enemy { get; set; }
+    }
+    public class EnemyCollection
+    {
+        public List<Enemy> enemy { get; set; }
+    }
+    public class Enemy
     {
         public string name { get; set; }
-        public List<int> Enemy { get; set; }
+        public int health { get; set; }
+        public int max_damage { get; set; }
+        public int min_damage { get; set; }
+        public int id { get; set; }
     }
 
     public class Program
     {
         static bool welcome = true, run = false;
 
-        static player Player = new player()
+        static void Main(string[] args)
         {
-            name = "null",
-            health = 100,
-            max_damage = 50,
-            min_damage = 10
-        };
-        static string playerJson = JsonSerializer.Serialize<player>(Player);
-        player p1 = JsonSerializer.Deserialize<player>(playerJson);
-
-        static enemies Enemy = new enemies()
-        {
-            name = "Enemy 1",
-            Enemy = new List<int>()
-        };
-        static string enemyJson = JsonSerializer.Serialize<enemies>(Enemy);
-        enemies e1 = JsonSerializer.Deserialize<enemies>(enemyJson);
-
-
-
-        public static void Main(string[] args)
-        {
-            Welcome();
             Loading();
+            Welcome();
         }
 
         public static void Loading()
@@ -68,13 +61,20 @@ namespace fightinggame
 
         static void Welcome()
         {
+            string propertyData = File.ReadAllText(@"..\properties.json");
+            User deserializedPlayer = JsonSerializer.Deserialize<User>(propertyData);
+            EnemyTypes deserializedEnemies = JsonSerializer.Deserialize<EnemyTypes>(propertyData);
+
+            Player p = deserializedPlayer.player;
+            EnemyCollection ec = deserializedEnemies.enemy;
+
             //Spelaren väljer sitt namn
             Console.WriteLine("Welcome to Fighting Game 101!");
             Console.WriteLine();
             Console.WriteLine("Choose your name:");
-            Player.name = Console.ReadLine();
+            p.name = Console.ReadLine();
             
-            Console.WriteLine($"{Player.name} VS {Enemy.name}");
+            Console.WriteLine($"{p.name} VS {ec.enemy[0].name}");
             Console.WriteLine();
             Console.WriteLine();
 
@@ -87,10 +87,6 @@ namespace fightinggame
 
         static void Run()
         {
-            // EnemyCollection bertil = JsonSerializer.Deserialize<EnemyCollection>(File.ReadAllText(@"..\properties.json"));
-            // Enemy e1 = bertil.enemies[0];
-            // Enemy e2 = bertil.enemies[1];
-
             Console.WriteLine("Ready...");
             Thread.Sleep(TimeSpan.FromSeconds(1));
             Console.WriteLine("Set...");
@@ -100,45 +96,48 @@ namespace fightinggame
             Console.WriteLine();
 
             // Vad som händer så länge en spelare är vid liv
-            while (Player.health > 0 && Enemy > 0)
-            {
-                Random rdm = new Random();
+            // while (Player.health > 0 && Enemy > 0)
+            // {
+            //     Random rdm = new Random();
 
-                //Slumpar skada som spelarna gör på varandra, visar sedan hur mycket skade de gjorde på varandra samt hur mycket hp de har kvar.
-                int p1dmg = rdm.Next(1, 41), p2dmg = rdm.Next(1, 41);
-                p1HP -= p2dmg;
-                p2HP -= p1dmg;
+            //     //Slumpar skada som spelarna gör på varandra, visar sedan hur mycket skade de gjorde på varandra samt hur mycket hp de har kvar.
+            //     int p1dmg = rdm.Next(1, 41), p2dmg = rdm.Next(1, 41);
+            //     p1HP -= p2dmg;
+            //     p2HP -= p1dmg;
 
-                Console.WriteLine($"{name1} dealt {p1dmg} damage to {name2}!");
-                Console.WriteLine($"{name2} dealt {p2dmg} damage to {name1}!");
-                Console.WriteLine();
+            //     Console.WriteLine($"{name1} dealt {p1dmg} damage to {name2}!");
+            //     Console.WriteLine($"{name2} dealt {p2dmg} damage to {name1}!");
+            //     Console.WriteLine();
 
-                if (p1HP < 0)
-                {
-                    p1HP = 0;
-                } else if (p2HP < 0)
-                {
-                    p2HP = 0;
-                }
+            //     if (p1HP < 0)
+            //     {
+            //         p1HP = 0;
+            //     }
+            //     else if (p2HP < 0)
+            //     {
+            //         p2HP = 0;
+            //     }
 
-                Console.WriteLine($"{name1}'s health: {p1HP}");
-                Console.WriteLine($"{name2}'s health: {p2HP}");
-                Console.WriteLine();
-                Console.WriteLine();
-                Thread.Sleep(TimeSpan.FromSeconds(3));
-            }
+            //     Console.WriteLine($"{name1}'s health: {p1HP}");
+            //     Console.WriteLine($"{name2}'s health: {p2HP}");
+            //     Console.WriteLine();
+            //     Console.WriteLine();
+            //     Thread.Sleep(TimeSpan.FromSeconds(3));
+            // }
 
-            //Vad som händer beroende på vem det är som vinner.
-            if (p1HP > 0)
-            {
-                Console.WriteLine($"{name1} won with {p1HP} HP left!");
-            } else if (p2HP > 0)
-            {
-                Console.WriteLine($"{name2} won with {p2HP} HP left!");
-            } else if (p1HP <= 0 && p2HP <= 0)
-            {
-                Console.WriteLine($"{name1} and {name2} killed each other, it's a draw!");
-            }
+            // //Vad som händer beroende på vem det är som vinner.
+            // if (p1HP > 0)
+            // {
+            //     Console.WriteLine($"{name1} won with {p1HP} HP left!");
+            // }
+            // else if (p2HP > 0)
+            // {
+            //     Console.WriteLine($"{name2} won with {p2HP} HP left!");
+            // }
+            // else if (p1HP <= 0 && p2HP <= 0)
+            // {
+            //     Console.WriteLine($"{name1} and {name2} killed each other, it's a draw!");
+            // }
 
 
             Console.ReadLine();
