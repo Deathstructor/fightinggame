2f85293c2f4d1799f734723a3c8202c0a2cfec9a
Paul Chastain <paulchast1@gmail.com>
2021-11-06 14:53:24 +0100

    Savegames

--- fightinggame/Program.cs
+++ fightinggame/Program.cs
@@ -131,28 +131,186 @@ namespace fightinggame
                         switch (selected)
                         {
                             case 1:
-                                NewGame();
-                                Console.Clear();
+                                SelectNewSave();
                                 break;
 
                             case 2:
                                 // LoadSaveGame();
-                                Console.Clear();
                                 break;
 
                             case 3:
                                 // LevelSelector();
-                                Console.Clear();
                                 break;
                         }
                         break;
                 }
-            Console.Clear();
+                Console.Clear();
             } while (true);
         }
 
+
+
+        static void SelectNewSave()
+        {
+            Console.Clear();
+
+            int selected = 1;
+            var ch = ConsoleKey.B;
+            do
+            {
+                Console.WriteLine("Select savegame slot");
+                Console.WriteLine();
+                Console.WriteLine();
+
+                // Kollar vilken rad man är på och lägger till pilen framför texten på raden man är på.
+                if (selected == 1)
+                {
+                    Console.WriteLine("> Savegame 1");
+                }
+                else
+                {
+                    Console.WriteLine("Savegame 1");
+
+                }
+                if (selected == 2)
+                {
+                    Console.WriteLine("> Savegame 2");
+                }
+                else
+                {
+                    Console.WriteLine("Savegame 2");
+
+                }
+                if (selected == 3)
+                {
+                    Console.WriteLine("> Savegame 3");
+                }
+                else
+                {
+                    Console.WriteLine("Savegame 3");
+                }
+
+                Console.WriteLine();
+                Console.ForegroundColor = ConsoleColor.DarkGray;
+                Console.WriteLine("Use the arrow keys to navigate and press enter to select.");
+                Console.ForegroundColor = ConsoleColor.White;
+
+                ch = Console.ReadKey(true).Key;
+
+                switch (ch)
+                {
+                    // Flyttar ner pilen om man trycker på "Down Arrow" och flyttar den högst upp
+                    // om pilen är längst ner.
+                    case ConsoleKey.DownArrow:
+                        if (selected == 3)
+                        {
+                            selected = 1;
+                        }
+                        else
+                        {
+                            selected++;
+                        }
+                        break;
+
+                    // Samma som ovan, fast med "Up Arrow" istället.
+                    case ConsoleKey.UpArrow:
+                        if (selected == 1)
+                        {
+                            selected = 3;
+                        }
+                        else
+                        {
+                            selected--;
+                        }
+                        break;
+
+                    // Laddar in metod när man trycker på ENTER beroende på vilken sak i
+                    // menyn man har valt.
+                    case ConsoleKey.Enter:
+                        ch = ConsoleKey.Insert;
+                        switch (selected)
+                        {
+                            case 1:
+                                while (ch != ConsoleKey.Y || ch != ConsoleKey.N)
+                                {
+                                    Console.WriteLine("Are you sure that you want to overwrite this save? (y/n)");
+                                    Console.WriteLine();
+                                    ch = Console.ReadKey(true).Key;
+                                    if (ch == ConsoleKey.Y)
+                                    {
+                                        File.WriteAllText(@"..\Savegames\Save1", "Successfully Created Savefile");
+                                        Console.WriteLine();
+                                        Console.WriteLine();
+                                        Console.WriteLine("Successfully Created Savefile");
+                                        Thread.Sleep(TimeSpan.FromSeconds(2));
+                                        NewGame();
+                                    }
+                                    else
+                                    {
+                                        Console.Clear();
+                                        Menu();
+                                    }
+                                }
+                                break;
+
+                            case 2:
+                                while (ch != ConsoleKey.Y || ch != ConsoleKey.N)
+                                {
+                                    Console.WriteLine("Are you sure that you want to overwrite this save? (y/n)");
+                                    Console.WriteLine();
+                                    ch = Console.ReadKey(true).Key;
+                                    if (ch == ConsoleKey.Y)
+                                    {
+                                        File.WriteAllText(@"..\Savegames\Save2", "Successfully Created Savefile");
+                                        Console.WriteLine();
+                                        Console.WriteLine();
+                                        Console.WriteLine("Successfully Created Savefile");
+                                        Thread.Sleep(TimeSpan.FromSeconds(2));
+                                        NewGame();
+                                    }
+                                    else
+                                    {
+                                        Console.Clear();
+                                        Menu();
+                                    }
+                                }
+                                break;
+
+                            case 3:
+                                while (ch != ConsoleKey.Y || ch != ConsoleKey.N)
+                                {
+                                    Console.WriteLine("Are you sure that you want to overwrite this save? (y/n)");
+                                    Console.WriteLine();
+                                    ch = Console.ReadKey(true).Key;
+                                    if (ch == ConsoleKey.Y)
+                                    {
+                                        File.WriteAllText(@"..\Savegames\Save3", "Successfully Created Savefile");
+                                        Console.WriteLine();
+                                        Console.WriteLine();
+                                        Console.WriteLine("Successfully Created Savefile");
+                                        Thread.Sleep(TimeSpan.FromSeconds(2));
+                                        NewGame();
+                                    }
+                                    else
+                                    {
+                                        Console.Clear();
+                                        Menu();
+                                    }
+                                }
+                                break;
+                        }
+                        break;
+                }
+                Console.Clear();
+            } while (true);
+        }
+
+
+
         static void NewGame()
         {
+            Console.Clear();
+
             string propertyData = File.ReadAllText(@"..\properties.json");
             User deserializedPlayer = JsonSerializer.Deserialize<User>(propertyData);
             EnemyTypes deserializedEnemies = JsonSerializer.Deserialize<EnemyTypes>(propertyData);
@@ -160,11 +318,9 @@ namespace fightinggame
             Player p = deserializedPlayer.player;
             EnemyCollection ec = deserializedEnemies.enemy;
 
-            Console.ForegroundColor = ConsoleColor.DarkGray;
-            Console.WriteLine("If you want to go back to the man menu press \"Left Arrow\".");
-            Console.ForegroundColor = ConsoleColor.White;
-
-            
+            // Console.ForegroundColor = ConsoleColor.DarkGray;
+            // Console.WriteLine("If you want to go back to the man menu press \"Left Arrow\".");
+            // Console.ForegroundColor = ConsoleColor.White;
 
             if (p.name == "undefined")
             {
