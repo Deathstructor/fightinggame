d8bd6cd180956209ad89eb9f8e30fba3e51cb52d
Paul Chastain <paulchast1@gmail.com>
2021-11-19 12:16:15 +0100

    la till spells och vapen i butiken, i json

--- fightinggame/JsonClasses.cs
+++ fightinggame/JsonClasses.cs
@@ -3,7 +3,7 @@ using System.Text.Json.Serialization;
 
 namespace fightinggame
 {
-    // Klasser som beskriver objekt för att serialisera och deserialisera "properties.json".
+    // Klasser som beskriver objekt för att serialisera och deserialisera "properties.json" eller savegames.
     public class User
     {
         public Player player { get; set; }
@@ -12,9 +12,6 @@ namespace fightinggame
     {
         public string name { get; set; }
         public int health { get; set; }
-        public int max_damage { get; set; }
-        public int min_damage { get; set; }
-        public double accuracy { get; set; }
         public int gold { get; set; }
         public int level { get; set; }
     }
@@ -43,10 +40,46 @@ namespace fightinggame
     }
 
 
+
     public class Types
     {
         public Player player { get; set; }
         [JsonPropertyName("enemies")]
         public EnemyCollection enemy { get; set; }
+    }
+
+
+
+    public class Shop
+    {
+        [JsonPropertyName("shop")]
+        public ShopCollection spellItems { get; set; }
+        public ShopCollection weaponItems { get; set; }
+    }
+    public class ShopCollection
+    {
+        public List<Spells> spellItems { get; set; }
+        public List<Weapons> weaponItems { get; set; }
+
+    }
+    public class Spells
+    {
+        public string name { get; set; }
+        public int health { get; set; }
+        public int damage { get; set; }
+        public double crit_multiplier { get; set; }
+        public double gold_multiplier { get; set; }
+        public int inventory { get; set; }
+        public string description { get; set; }
+    }
+    public class Weapons
+    {
+        public string name { get; set; }
+        public int max_damage { get; set; }
+        public int min_damage { get; set; }
+        public double accuracy { get; set; }
+        public int price { get; set; }
+        public bool owned { get; set; }
+        public string description { get; set; }
     }
 }
